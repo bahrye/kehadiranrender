96b77c8e256affa049e98e1fb2404361f2bb5511
bahrye <112942911+bahrye@users.noreply.github.com>
2026-01-31 14:04:10 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: import.meta.env?.VITE_FIREBASE_API_KEY || "AIzaSyDT8CyigYFfoVg3DXunOq7pKvbMpTZkQ98",
+  authDomain: "cekloknewversion.firebaseapp.com",
+  projectId: "cekloknewversion",
+  storageBucket: "cekloknewversion.firebasestorage.app",
+  messagingSenderId: "602084220497",
+  appId: "1:602084220497:web:ac20adef8bb50acef56270"
+};
